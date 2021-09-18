ed97f15628e06bde3eaf349062a6334cc120b606
charlie1099 <cdcharlesworth@gmail.com>
2021-09-18 11:13:11 -0600

    added working buttons

--- assets/js/game.js
+++ assets/js/game.js
@@ -6,11 +6,22 @@ var questions = [
     {q: "Where is a good website to go for coding questions?", a: "A. Petsmart.com B. Amizon.com C. Stackoverflow.com D. trufflesuite.com", answer:"C"},
     {q: "When coding in HTML how do you start/open a line of code?", a: "A. [] B. {} C. :: D. <>", answer:"D"}
 ]
-var  buttonEl = addEventListener(click, "#start")
-pageContentEl.quirySelector("#start");
-    console.log("#start")
+// var  buttonEl = addEventListener(click, "#start")
+// pageContentEl.querySelector("#start");
+//     console.log("#start")
 
-var pageContentEl = "#start"
+var fullQuestion = document.querySelector("#quiz-questions")
+
+var startBtn = document.querySelector("#start")
+ console.log(startBtn)
+ 
+
+var startQuiz = function() {
+    console.log("started")
+    fullQuestion.style.display = "flex"
+} 
+startBtn.addEventListener("click", startQuiz)
+
 // make a variable to keep score
 var score = 0;
 //make loop to go though the questions
@@ -19,6 +30,6 @@ var score = 0;
 
 //make it so that when the game is over or if the time is out then the game ends
 // make the player able to save there score in local storage at end of game
-/* I relize that this in not done. I just wnated to turn in at lest somthing in on time becuse the last projest
-that I turned in late said that it was not going to be added to my finale score. I will be turning in a complet 
+/* I realize that this in not done. I just wanted to turn in at lest somthing in on time becuse the last projest
+that I turned in late said that it was not going to be added to my finale score. I will be turning in a complet
 project in a few days. */
