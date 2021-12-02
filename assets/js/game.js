093d214477e2f94825509e02245c87531f036177
charlie1099 <cdcharlesworth@gmail.com>
2021-12-02 12:09:08 -0700

    fixing project

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,35 +1,167 @@
-// make question 1-4 store as arrys list in objects use random 
-//to make questions cycal though use a for loop to go though the questions
+var pos = 0;
+var right = 0;
+var test, test_status, questions, choice, choices, chA, chB, chC, chD;
+var  initalInput = document.querySelector("#initialsLable")
+var isPlaying = true 
+
 var questions = [
-    {q: "Var is short for what?", a: "A. variable B. varloom C. varroom D. varney", answer:"A"  },
-    {q: "What Does CSS stand for?", a: "A. Capton Sam See B. Computer Screen Speed C. Cascading Style Sheet D. Computer Sea Side", answer:"C" },
-    {q: "Where is a good website to go for coding questions?", a: "A. Petsmart.com B. Amizon.com C. Stackoverflow.com D. trufflesuite.com", answer:"C"},
-    {q: "When coding in HTML how do you start/open a line of code?", a: "A. [] B. {} C. :: D. <>", answer:"D"}
+    {
+        question: "Var is short for what?",
+        a: 'variable',
+        b: 'varloom',
+        c: 'varroom',
+        d: 'varney',
+        answer: "A"
+    },
+    {
+        question: "What Does CSS stand for?",
+        a: "Capton Sam See",
+        b: "Computer Screen Speed",
+        c: "Cascading Style Sheet",
+        d: "Computer Sea Side",
+        answer: "C"
+    },
+    {
+        question: "Where is a good website to go for coding questions?",
+        a: "Petsmart.com",
+        b: "Amizon.com",
+        c: "Stackoverflow.com",
+        d: "trufflesuite.com",
+        answer: "C"
+    },
+    {
+        question: "When coding in HTML how do you start/open a line of code?",
+        a: "[]",
+        b: "{}",
+        c: "::",
+        d: "<>",
+        answer: "D"
+    }
 ]
-// var  buttonEl = addEventListener(click, "#start")
-// pageContentEl.querySelector("#start");
-//     console.log("#start")
+// this will get the questions
+function get(x) {
+    return document.getElementById(x)
+}
 
-var fullQuestion = document.querySelector("#quiz-questions")
+function reload() {
+    window.location.reload();
+}
 
-var startBtn = document.querySelector("#start")
- console.log(startBtn)
+//this will render a question to display on the page
+function renderQuestion() {
+    test = get("test")
+    if(pos >= questions.length) {
+        isPlaying = false;
+        get("test_status").innerHTML = "All Done!.";
+        document.getElementById("test").innerHTML = "<p>Your Final Score is: "+(100/questions.length) * correct+".</p>"
+        test.innerHTML += "<p>Enter Your Initials to Save Your Score </p>"+"<label><input type='text' id='initialsLabel'></label>"+
+        "<button type='submit' id='initials'>Submit Initals</button>"
+        var initialsEl = document.getElementById("initials")
+        initialsEl.onclick = scoreBoard
+        //will allow the user to restart
+        pos = 0
+        correct = 0
+        return false
+    }
+
+    get("test_status").innerHTML = "Question "+(pos+1)+" of "+questions.length
+
+    question = questions[pos].question
+    chA = questions[pos].a
+    chA = questions[pos].b
+    chA = questions[pos].c
+    chA = questions[pos].d
+    // display the question and the anwsers
+    test.innerHTML += "<label> <input type='radio' name='choices' value='A'> "+chA+"</label><br>"
+    test.innerHTML += "<label> <input type='radio' name='choices' value='B'> "+chB+"</label><br>"
+    test.innerHTML += "<label> <input type='radio' name='choices' value='C'> "+chC+"</label><br>"
+    test.innerHTML += "<label> <input type='radio' name='choices' value='D'> "+chD+"</label><br>"
+    test.innerHTML += "<button onclick= 'checkAnswer()'>Submit Anwser</button>"
+
+}
+
+// checks user input for the anwers
+function checkAnswer() {
+    choices = document.getElementsByName("choices");
+    for(var i = 0; i<choices.length; i++){
+        if(choices[i].checked){
+            choice = choices[i].value;
+        }
+    }
+    // see if the input is the right one
+    if(choice == questions[pos].answer){
+        correct++
+        console.log(correct)
+    }
+    pos++
+    localStorage.setItem("correctScore", ((100/questions.length)*correct))
+    renderQuestion()
+}
+
+var scoreBoardEl = document.getElementById("scores")
+
+function scoreBoard() {
+    var initialsLabelEl = document.getElementById("initialsLabel")
+    localStorage.setItem("name", initialsLabelEl)
+
+    get("test_status").innerHTML = "High Score"
+    test.innerHTML = "<h3>Your Score</h3>"+"<ul></ul>"
+    test.innerHTML += "<li>1. "+localStorage.getItem("name")+"</li>"+localStorage.getItem("correctScore")
+    
+    //button if they would like to play again
+    test.innerHTML += "<button type='submit' id='play-again'>Would You Like to Play Again?</button>"
+    var plaAgainEl = document.getElementById("play-again")
+    plaAgainEl.onclick = reload
+    
+}
+
+var count = 0
+//timer
+var timerEl = document.getElementById("time")
+var startEl = document.getElementById("start")
+// this will count down
+function countdown() {
+    var timeLeft = 50
+    var timeInterval = setInterval(function() {
+        if(!isPlaying) {
+            clearInterval(timeInterval)
+        } else if (timeLeft > 1) {
+            timerEl.textContent = timeLeft;
+            // decrease time by 1
+            timeLeft--
+        } else {
+            timerEl.textContent = ""
+            document.getElementById("test_status").innerHTML="Time is Out!!!"
+            document.getElementById("test").innerHTML="You Have Ran Out of Time!!!"+"<button type='submit' id='try-again'>Try Again</button>"
+
+            var tryAgainEl = document.getElementById("try-again")
+            tryAgainEl.onclick = reload
+
+            clearInterval(timeInterval)
+        }
+    }, 1000)
+
+}
+startEl.onclick = countdown
+
+startEl.addEventListener("click", renderQuestion)
+scoreBoardEl.onclick = scoreBoard
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
  
-
-var startQuiz = function() {
-    console.log("started")
-    fullQuestion.style.display = "flex"
-} 
-startBtn.addEventListener("click", startQuiz)
-
-// make a variable to keep score
-var score = 0;
-//make loop to go though the questions
-// when awser is wrong it will subtract time
-// make the multiple choose question buttons with append childs
-
-//make it so that when the game is over or if the time is out then the game ends
-// make the player able to save there score in local storage at end of game
-/* I realize that this in not done. I just wanted to turn in at lest somthing in on time becuse the last projest
-that I turned in late said that it was not going to be added to my finale score. I will be turning in a complet
-project in a few days. */
